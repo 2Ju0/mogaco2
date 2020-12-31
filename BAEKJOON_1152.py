a65eabbe94ae132272b5be761f9c76f5cf73f9ec
JuYeong-Lee <67141218+JuYeong-Lee@users.noreply.github.com>
2020-12-31 13:25:58 +0900

    BAEKJOON no.1152

--- BAEKJOON_1152.py
+++ BAEKJOON_1152.py
@@ -1,16 +1,7 @@
 str = input()
 cnt = 0
 
-if (len(str) == 0): # 아무것도 입력하지 않은 경우
-    print(0)
-    exit(0)
-
-str = list(str) # list로 자료구조를 변경해야 str[0] = "" 와 같이 할당 가능
-if (str[0] == " "):
-    str[0] = "" 
-if (str[len(str)-1] == " "):
-    str[len(str)-1] = ""
-str = ''.join(str) # ''를 구분자로 하여 문자열로 합치기
+str = str.strip()
 
 if (len(str) == 0): # 공백만 입력된 경우
     print(0)
@@ -22,6 +13,5 @@ for i in str:
 
 print(cnt + 1)
 
-# 더 좋은 알고리즘
-n = input().split() # my name is juyeong -- split --> ['my', 'name', 'is', 'juyeong']
-print(len(n))
+# n = input().split() # my name is juyeong -- split --> ['my', 'name', 'is', 'juyeong']
+# print(len(n))
